bf4adbe5b3a269ad1a2d1f53de2b4951e01ec1e0
Santiago Cortes <santi.lamarin98@gmail.com>
2022-02-28 17:18:51 -0500

    Creando proyecto MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace platizi_curso_aspcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
